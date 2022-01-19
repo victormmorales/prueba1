97fa13488726f23d6c348f3f801cce20237f237c
victormmorales <victorfasrev@gmail.com>
2022-01-19 13:52:53 +0100

    exports

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
+import HolaMundo from "./components/HolaMundo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Victor</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <HolaMundo />
       </header>
     </div>
   );
